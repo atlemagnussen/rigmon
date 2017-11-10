1e72f243d46a814ba6147ff3aaef83291bf4700e
Atle Magnussen <atlemagnussen@gmail.com>
2017-11-10 08:21:54 +0100

    working rpc implementation

--- rpc.js
+++ rpc.js
@@ -3,17 +3,55 @@ var net = require('net');
 class Rpc {
     constructor(params) {
         this.params = params;
-        this.socket = new net.Socket();
+        this.socket = new net.Socket()
+        .on('data', function(data) {
+            console.log(data);
+            var d = JSON.parse(data);
+            console.log(JSON.stringify(d,null,2));
+        })
+        .on('close', function() {
+            console.log('close');
+        })
+        .on('timeout', function() {
+            console.log('timeout');
+        })
+        .on('error', function() {
+            console.log('error');
+        });
     }
 
-    call(rpcCall) {
-        
-        connect();
+    call(req) {
+        this.socket.on('connect', function() {
+            console.log(': connected to ' + this.remoteAddress + ':' + this.remotePort);
+            this.write(req + '\n');
+            this.setTimeout(30);
+        });
+        this.connect();
     }
 
     connect() {
-        this.socket.connect(params.server, params.port);
+        console.log(`connect ${this.params.host}:${this.params.port}`);
+        this.socket.connect(this.params.port, this.params.host);
     }
 }
 
-module.exports = Rpc;
+//module.exports = Rpc;
+
+var req = '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}';
+var rig2 = {
+    "host": "57.57.57.6",
+    "port": 3333
+};
+var rig3 = {
+    "host": "57.57.57.7",
+    "port": 3333
+};
+var rpcrig3 = new Rpc(rig3);
+var rpcrig2 = new Rpc(rig2);
+
+function poll(){
+    rpcrig3.call(req);
+    rpcrig2.call(req);
+}
+
+poll();
