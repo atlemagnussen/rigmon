953de825ebe9217a4fbb9dcad1467ad209923686
Atle Magnussen <atlemagnussen@gmail.com>
2017-11-10 12:07:13 +0100

    try this

--- rpc.js
+++ rpc.js
@@ -1,47 +1,40 @@
 var net = require('net');
 
-class Rpc {
-    constructor() {
+var Rpc = {
+    Tpc: class tcp{
+        constructor(params) {
+            this.params = params;
+            this.socket = new net.Socket()
+            .on('data', function(data) {
+                console.log(data);
+                var d = JSON.parse(data);
+                console.log(JSON.stringify(d,null,2));
+            })
+            .on('close', function() {
+                console.log('close');
+            })
+            .on('timeout', function() {
+                console.log('timeout');
+            })
+            .on('error', function() {
+                console.log('error');
+            });
+        }
+        call(req) {
+            this.socket.on('connect', function() {
+                console.log(': connected to ' + this.remoteAddress + ':' + this.remotePort);
+                this.write(req + '\n');
+                this.setTimeout(30);
+            });
+            this.connect();
+        }
 
+        connect() {
+            console.log(`connect ${this.params.host}:${this.params.port}`);
+            this.socket.connect(this.params.port, this.params.host);
+        }
     }
-    get Tcp() {
-        return RpcTpc;
-    }
-}
-
-class RpcTpc {
-    constructor(params) {
-        this.params = params;
-        this.socket = new net.Socket()
-        .on('data', function(data) {
-            console.log(data);
-            var d = JSON.parse(data);
-            console.log(JSON.stringify(d,null,2));
-        })
-        .on('close', function() {
-            console.log('close');
-        })
-        .on('timeout', function() {
-            console.log('timeout');
-        })
-        .on('error', function() {
-            console.log('error');
-        });
-    }
-    call(req) {
-        this.socket.on('connect', function() {
-            console.log(': connected to ' + this.remoteAddress + ':' + this.remotePort);
-            this.write(req + '\n');
-            this.setTimeout(30);
-        });
-        this.connect();
-    }
-
-    connect() {
-        console.log(`connect ${this.params.host}:${this.params.port}`);
-        this.socket.connect(this.params.port, this.params.host);
-    }
-}
+};
 
 //module.exports = Rpc;
 
