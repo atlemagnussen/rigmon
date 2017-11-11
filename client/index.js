595f6804be1880b9cd907fb6459639602dbc4b3e
Atle Magnussen <atlemagnussen@gmail.com>
2017-11-12 00:54:48 +0100

    wrap rest in promise, add connect disconnect

--- client/index.js
+++ client/index.js
@@ -1,41 +1,51 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    var rest = new Rest(); // jshint ignore:line
     var btnRefresh = document.getElementById("btnRefresh");
+    var btnConnect = document.getElementById("btnConnect");
+    var btnDisconnect = document.getElementById("btnDisconnect");
     var textAreaOutput = document.getElementById("output");
+    btnConnect.addEventListener("click", function() {
+        initWs();
+    });
+    btnDisconnect.addEventListener("click", function() {
+        if (ws && ws.readyState === ws.OPEN) {
+            ws.close();
+        } else {
+            showMessage("web socket is not open. readyState=" + ws.readyState);
+        }
+    });
     btnRefresh.addEventListener("click", function() {
-        var oReq = new XMLHttpRequest();
-        oReq.addEventListener("load", reqListener);
-        oReq.open("GET", "api");
-        oReq.send();
+        rest.call("GET", "api")
+        .then(showMessage, showMessage);
     });
 
-    function reqListener() {
-        if (this.responseText) {
-            showMessage(this.responseText);
-        }
-    }
     function showMessage(msg) {
         textAreaOutput.value += "\n" + msg;
+        textAreaOutput.scrollTop = textAreaOutput.scrollHeight
     }
     let ws;
     function initWs() {
+        if (!ws || ws.readyState === ws.CLOSED) {
         ws = new WebSocket(`ws://${location.host}`);
-        ws.onerror = () => {
-            showMessage('WebSocket error');
-        };
-        ws.onopen = () => {
-            showMessage('WebSocket connection established');
-        };
-        ws.onclose = () => {
-            showMessage('WebSocket connection closed');
-        };
-        ws.onmessage = (msg) => {
-            showMessage(msg.data);
-        };
+            ws.onerror = () => {
+                showMessage('WebSocket error');
+            };
+            ws.onopen = () => {
+                showMessage('WebSocket connection established');
+            };
+            ws.onclose = () => {
+                showMessage('WebSocket connection closed');
+            };
+            ws.onmessage = (msg) => {
+                showMessage(msg.data);
+            };
+        } else {
+            showMessage("web socket is already open. readyState=" + ws.readyState);
+        }
     }
-    setTimeout(initWs, 2000);
+    // setTimeout(initWs, 2000);
 
     var loaded = "Loaded client " + new Date();
     showMessage(loaded);
---
+++ client/rest.js
@@ -0,0 +1,28 @@
+class Rest { // jshint ignore:line
+    constructor() {
+
+    }
+    call(method, url) {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+          if (this.status >= 200 && this.status < 300) {
+            resolve(xhr.response);
+          } else {
+            reject({
+              status: this.status,
+              statusText: xhr.statusText
+            });
+          }
+        };
+        xhr.onerror = function () {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText
+          });
+        };
+        xhr.send();
+        });
+    }
+}
