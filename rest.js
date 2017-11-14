1a56bc09d2031ee34bc90563c819ba240858f8f2
Atle Magnussen <atlemagnussen@gmail.com>
2017-11-14 11:12:29 +0100

    try without contenttype check

--- rest.js
+++ rest.js
@@ -22,15 +22,16 @@ class Rest extends Rig {
                 statusCode
             } = res;
             const contentType = res.headers['content-type'];
-
+            logger.debug("contentType" + contentType);
             let error;
             if (statusCode !== 200) {
                 error = new Error('Request Failed.\n' +
                     `Status Code: ${statusCode}`);
-            } else if (!/^application\/json/.test(contentType)) {
-                error = new Error('Invalid content-type.\n' +
-                    `Expected application/json but received ${contentType}`);
             }
+            // else if (!/^application\/json/.test(contentType)) {
+            //     error = new Error('Invalid content-type.\n' +
+            //         `Expected application/json but received ${contentType}`);
+            // }
             if (error) {
                 logger.error(error.message);
                 // consume response data to free up memory
